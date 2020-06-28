667f82290f101db3c9322ce143f4c197a74bdf64
jlferrete <joseluisferrete@gmail.com>
2020-06-28 10:35:59 +0200

    Initial commit

---
+++ src/test/java/com/sdos/springboot/webflux/app/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdos.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
