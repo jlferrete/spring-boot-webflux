3151802d58258f23b213de672876d206df12e0cb
jlferrete <joseluisferrete@gmail.com>
2020-06-28 11:03:18 +0200

    Project done without API REST

---
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -0,0 +1,55 @@
+package com.bolsadeideas.springboot.webflux.app;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+
+import com.bolsadeideas.springboot.webflux.app.models.dao.ProductoDao;
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootWebfluxApplication implements CommandLineRunner{
+
+	@Autowired
+	private ProductoDao dao;
+	
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		mongoTemplate.dropCollection("productos").subscribe();
+		
+		Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.89),
+				new Producto("Sony Camara HD Digital", 177.89),
+				new Producto("Apple iPod", 46.89),
+				new Producto("Sony Notebook", 846.89),
+				new Producto("Hewlett Packard Multifuncional", 200.89),
+				new Producto("Bianchi Bicicleta", 70.89),
+				new Producto("HP Notebook Omen 17", 2500.89),
+				new Producto("Mica Cómoda 5 Cajones", 150.89),
+				new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89)
+				)
+		.flatMap(producto -> {
+			producto.setCreateAt(new Date());
+			return dao.save(producto);
+			})
+		.subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
+		
+	}
+}
---
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,86 @@
+package com.bolsadeideas.springboot.webflux.app.controllers;
+
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+
+import com.bolsadeideas.springboot.webflux.app.models.dao.ProductoDao;
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+
+@Controller
+public class ProductoController {
+
+	@Autowired
+	private ProductoDao dao;
+	
+	private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+	
+	@GetMapping({"/listar", "/"})
+	public String listar(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		});
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 1));
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
+	
+	@GetMapping("/listar-full")
+	public String listarFull(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).repeat(5000);
+		
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
+	
+	@GetMapping("/listar-chunked")
+	public String listarChunked(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).repeat(5000);
+		
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar-chunked";
+	}
+}
